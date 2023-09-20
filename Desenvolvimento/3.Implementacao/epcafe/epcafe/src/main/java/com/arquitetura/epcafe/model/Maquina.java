24104560e0cf8e7ec55e9b5911117807b0df3134
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2023-09-20 10:29:19 -0300

    entidades Maquina e Usuario

--- Desenvolvimento/3.Implementacao/epcafe/epcafe/src/main/java/com/arquitetura/epcafe/model/Maquina.java
+++ Desenvolvimento/3.Implementacao/epcafe/epcafe/src/main/java/com/arquitetura/epcafe/model/Maquina.java
@@ -1,5 +1,20 @@
 package com.arquitetura.epcafe.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Maquina {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
 }
